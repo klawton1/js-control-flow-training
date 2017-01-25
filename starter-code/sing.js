38c6d5a7958101422d6d2d78d550aa6c10e8c2fb
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2017-01-24 21:43:15 -0800

    added sing.js loop

--- starter-code/sing.js
+++ starter-code/sing.js
@@ -1,1 +1,20 @@
 console.log("sing.js loaded");
+var bottles = Number(prompt("how many bottles of beer?"));
+while(bottles > 0) {
+	if (bottles === 1) {
+		console.log(bottles + ' bottle of beer on the wall,');
+		console.log(bottles + ' bottle of beer!');
+		console.log('Take one down and pass it around,');
+		console.log('No bottles of beer on the wall!');
+		break;
+	}
+	console.log(bottles + ' bottles of beer on the wall,');
+	console.log(bottles + ' bottles of beer!');
+	console.log('Take one down and pass it around,');
+	bottles --;
+	if (bottles === 1) {
+	console.log(bottles + ' bottle of beer on the wall!');
+	continue;
+	}
+	console.log(bottles + ' bottles of beer on the wall!');
+}
